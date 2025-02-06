fbb942fcc2e873b559294754e45accd69a39107b
Max Mezalon <Max.Mezalon@blackstone.com>
2025-02-06 10:20:15 -0500

    deep copy and the this keyword

--- advance.js
+++ advance.js
@@ -140,3 +140,36 @@ findPriceExtremes([
   ...averageSharePriceByMonthQ3,
   ...averageSharePriceByMonthQ4,
 ]);
+
+// deep clone objects
+const studentDetails = {
+  firstName: "janaka",
+  lastName: "siriwardena",
+  age: 28,
+  country: "sri lanka",
+  email: "j.siri@totalinternet.com",
+  discordUsername: "JS1",
+  modulesCompleted: ["html", "js", "css"],
+};
+
+const deepClonedStudentDetails = structuredClone(studentDetails);
+deepClonedStudentDetails.modulesCompleted[0] = "TS";
+console.log(studentDetails);
+console.log(deepClonedStudentDetails);
+
+// the bind 'this' method
+const product = {
+  name: "Vanilla Lip Gloss",
+  sku: "w234fg",
+  stock: 276,
+  getProductInfo: function () {
+    console.log(this);
+    // console.log(`Stock level for ${this.name} (SKU: ${this.sku}): ${this.stock}`)
+  },
+};
+
+// product.getProductInfo()
+
+const productDetails = product.getProductInfo.bind(product); // without the bind(product) - 'this' would be undefined
+
+productDetails();
